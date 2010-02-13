a679d11b415dd352d4c623c52bbebaae7d23a581
Watson <watson1978@gmail.com>
2010-02-13 13:09:34 +0900

    Add sample file.

--- sample3.rb
+++ sample3.rb
@@ -21,8 +21,6 @@ data = (1..DATA_SIZE).to_a
 input  = opencl.set_input(data, DATA_SIZE)
 output = opencl.set_output(DATA_SIZE)
 
-opencl.sum(input, output, DATA_SIZE)
-
 Benchmark.bmbm do |x|
   x.report("map        ") { data.map {|x| (x.to_f + 0.5) / 3.8 + 2.0 } }
   x.report("OpenCL(cpu)") { opencl.sum(input, output, DATA_SIZE) }
